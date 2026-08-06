e441ed3d752eaae5bb378db974c6a47be63a59b4
Pallavi Verma <2300521520040@ietlucknow.ac.in>
2026-08-07 00:48:39 +0530

    Remove stray root files from repository root

--- PDFDownloadLinkWrapper.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { PDFDownloadLink, type PDFDownloadLinkProps } from "@react-pdf/renderer";
-
-export default function PDFDownloadLinkWrapper(props: PDFDownloadLinkProps) {
-  return <PDFDownloadLink {...props} />;
-}
--- PDFViewerWrapper.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { PDFViewer, type PDFViewerProps } from "@react-pdf/renderer";
-
-export default function PDFViewerWrapper(props: PDFViewerProps) {
-  return <PDFViewer {...props} />;
-}
